c7d629c1281d364e274c15b95d9c2a669c7f3023
wiusxt <ashatovanas65@gmail.com>
2022-01-31 09:26:23 +0600

    added first function

---
+++ script.js
@@ -0,0 +1,9 @@
+// very simple function
+// playCards
+// cookDinner
+function callFriend() {
+console.log('Pick up the phone');
+console.log('Type in the number');
+console.log('Press call');
+console.log('wait for the answer');
+}
